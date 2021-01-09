678981584fd55f8b679d114e0f214eb728917507
Lee Doughty <leedoughtydesign@gmail.com>
2021-01-09 15:47:02 +0000

    Add more operators, methods and keywords exercises

---
+++ microsoft-exam/operators-methods-keywords/browser-object-model/display-dialogs/script.js
@@ -0,0 +1,10 @@
+alert("Hello, this is an alert");
+// no return value
+
+confirm("Hello, this is a confirm");
+// true (if the user clicks "OK");
+// false (if the user clicks "Cancel");
+
+prompt("Hello, this is a prompt");
+// input value (if the user clicks "OK");
+// null (if the user clicks "cancel");
---
+++ microsoft-exam/operators-methods-keywords/browser-object-model/screen-size/script.js
@@ -0,0 +1,5 @@
+console.log(screen.width);
+console.log(screen.height);
+
+console.log(window.innerWidth);
+console.log(window.innerHeight);
